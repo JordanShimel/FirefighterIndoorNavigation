ebc4fa1656bb57a076771e76f344ac891370dc4b
Thomas Manion Anter <fz5911@wayne.edu>
2019-10-28 22:10:52 -0400

    Upload New File

---
+++ pointcloudWidget.cpp
@@ -0,0 +1,43 @@
+//pointcloudWidget code file
+//pointcloudWidget handles pointcloud functions, including:
+//  creating pointcloud from bag
+//  displaying pointcloud in ui
+//  allowing manipulation of pointcloud view
+//  creating static images of pointcloud to be returned to remote unit(s)
+
+#include "pointcloudWidget.hpp"
+
+pointcloudWidget::pointcloudWidget()
+{
+
+}
+
+pointcloudWidget::~pointcloudWidget()
+{
+
+}
+
+void pointcloudWidget::buildPointcloud()
+{
+
+}
+
+void pointcloudWidget::displayPointcloud()
+{
+
+}
+
+void pointcloudWidget::startManipulatingPointcloud()
+{
+
+}
+
+void pointcloudWidget::stopManipulatingPointcloud()
+{
+
+}
+
+void pointcloudWidget::capturePointcloud()
+{
+
+}
