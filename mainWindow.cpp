cd26a8321642770c139748c262186904d8c922e4
Thomas Anter <fz5911@wayne.edu>
2019-11-12 16:48:29 -0800

    Added new widget for future point cloud integration

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -7,23 +7,54 @@
 //creates ui instance
 //initializes ui elements
 //connects Qt Signals used by the application
+
+
+
+
+
 mainWindow::mainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::mainWindow)
 {
     //initialize UI
     ui->setupUi(this);
 
-    QWebEngineView* viewDepthData;
-    viewDepthData = new QWebEngineView(this);
-    viewDepthData->load(QUrl("http://localhost:8080/stream_viewer?topic=/rscDepth"));
-    viewDepthData->setZoomFactor(0.68);
-    viewDepthData->page()->setBackgroundColor((Qt::transparent));
-    viewDepthData->show();
-    ui->tabCamera1VideoStream->addWidget(viewDepthData);
+    initVidView();
 
-    QPixmap pix("/home/jordan/Projects/firefighterbaseunit/PC.jpg");
-    ui->labelpc->setPixmap(pix);
+    initPCView();
 
+    initRosNodes();
+
+}
+
+
+void mainWindow::initPCView()
+{
+    // create widget
+    QOpenGLWidget* viewPC;
+    viewPC = new QOpenGLWidget(this);
+    // create variable for context
+    QOpenGLContext* openGLContext;
+    // get context from widget
+    openGLContext = viewPC->context();
+
+    // put widget in UI
+
+    // possible way
+    // glGetString: return a string describing the current GL connection
+    const GLubyte* glGetString(GLenum name);
+
+    ui->tabCamera1PC->addWidget(viewPC);
+
+
+    //void glBind*(GLenum target​, GLuint object​); saving this for reference
+
+}
+
+
+
+void mainWindow::initRosNodes()
+{
     //connection to allow application to close rather than hang if ROS has to shutdown unexpectedly
+
     QObject::connect(&rosNode, SIGNAL(rosShutdown()), this, SLOT(close()));
 
     //connection to detect receipt of new acceleration data
@@ -33,8 +64,21 @@ mainWindow::mainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::mainWi
     //connection to detect receipt of new gyroscope data
     //TODO:implement
 
+
     //temporary location for logic to start ROS node
     rosNode.init("http://localhost:11311/", "localhost");
+}
+
+
+void mainWindow::initVidView()
+{
+    QWebEngineView* viewDepthData;
+    viewDepthData = new QWebEngineView(this);
+    viewDepthData->load(QUrl("http://localhost:8080/stream_viewer?topic=/rscDepth"));
+    viewDepthData->setZoomFactor(0.68);
+    viewDepthData->page()->setBackgroundColor((Qt::transparent));
+    viewDepthData->show();
+    ui->tabCamera1VideoStream->addWidget(viewDepthData);
 }
 
 //public: destructor
@@ -42,6 +86,11 @@ mainWindow::mainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::mainWi
 mainWindow::~mainWindow()
 {
     delete ui;
+}
+
+void mainWindow::updatePointCloud()
+{
+
 }
 
 void mainWindow::updateDepth()
